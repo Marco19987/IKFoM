3bb31684572cbe3124185537bda3a4aa19db005e
Marco De Simone <marco.desimone@unicampania.it>
2025-06-02 11:57:32 +0200

    working simple test

--- test_ikfom/include/test_ikfom/use-ikfom.hpp
+++ test_ikfom/include/test_ikfom/use-ikfom.hpp
@@ -4,51 +4,39 @@
 
 #include "../IKFoM_toolkit/esekfom/esekfom.hpp"
 
-
 typedef MTK::vect<3, double> vect3;
 typedef MTK::SO3<double> SO3;
 
-
 MTK_BUILD_MANIFOLD(state_ikfom,
-((vect3, bpo)) 
-((SO3, bQo))
-);
+				   ((vect3, bpo))((SO3, bQo)));
 
 MTK_BUILD_MANIFOLD(input_ikfom,
-((vect3, ovo))
-((vect3, oomegao))
-);
+				   ((vect3, ovo))((vect3, oomegao)));
 
 MTK_BUILD_MANIFOLD(process_noise_ikfom,
-((vect3, n_ovo))
-((vect3, n_oomegao))
-);
+				   ((vect3, n_ovo))((vect3, n_oomegao)));
 
 MTK_BUILD_MANIFOLD(measurement_ikfom,
-((vect3, bpo_meas)) 
-((SO3, bQo_meas))
-);
+				   ((vect3, bpo_meas))((SO3, bQo_meas)));
 
 MTK_BUILD_MANIFOLD(measurement_noise_ikfom,
-((vect3, v_bpo))
-((vect3, v_bQo))
-);
+				   ((vect3, v_bpo))((vect3, v_bQo)));
 
 MTK::get_cov<process_noise_ikfom>::type process_noise_cov()
 {
 	MTK::get_cov<process_noise_ikfom>::type cov = MTK::get_cov<process_noise_ikfom>::type::Zero();
-	MTK::setDiagonal<process_noise_ikfom, vect3, 0>(cov, &process_noise_ikfom::n_ovo, 0.0001);// 0.03
+	MTK::setDiagonal<process_noise_ikfom, vect3, 0>(cov, &process_noise_ikfom::n_ovo, 0.0001);	   // 0.03
 	MTK::setDiagonal<process_noise_ikfom, vect3, 3>(cov, &process_noise_ikfom::n_oomegao, 0.0001); // *dt 0.01 0.01 * dt * dt 0.05
-	
+
 	return cov;
 }
 
 MTK::get_cov<measurement_noise_ikfom>::type measurement_noise_cov()
 {
 	MTK::get_cov<measurement_noise_ikfom>::type cov = MTK::get_cov<measurement_noise_ikfom>::type::Zero();
-	MTK::setDiagonal<measurement_noise_ikfom, vect3, 0>(cov, &measurement_noise_ikfom::v_bpo, 0.0001);// 0.03
+	MTK::setDiagonal<measurement_noise_ikfom, vect3, 0>(cov, &measurement_noise_ikfom::v_bpo, 0.0001); // 0.03
 	MTK::setDiagonal<measurement_noise_ikfom, vect3, 3>(cov, &measurement_noise_ikfom::v_bQo, 0.0001); // *dt 0.01 0.01 * dt * dt 0.05
-	
+
 	return cov;
 }
 
@@ -57,48 +45,33 @@ MTK::get_cov<measurement_noise_ikfom>::type measurement_noise_cov()
 #define measurement_dof 6 //! zk = h(xk,vk) the dof is not the dim of zk
 #define measurement_noise_dof 6
 
-
 Eigen::Matrix<double, state_dof, 1> get_f(state_ikfom &s, const input_ikfom &in)
 {
 	Eigen::Matrix<double, state_dof, 1> res = Eigen::Matrix<double, state_dof, 1>::Zero();
-	res.template block<3,1>(0,0) = (s.bQo * Eigen::Quaternion<double>(0, in.ovo[0], in.ovo[1], in.ovo[2]) * s.bQo.conjugate()).vec();	
-	res.template block<3,1>(3,0) = in.oomegao;
+	res.template block<3, 1>(0, 0) = (s.bQo * Eigen::Quaternion<double>(0, in.ovo[0], in.ovo[1], in.ovo[2]) * s.bQo.conjugate()).vec();
+	res.template block<3, 1>(3, 0) = in.oomegao;
 	return res;
 }
 
 Eigen::Matrix<double, state_dof, state_dof> df_dx(state_ikfom &s, const input_ikfom &in)
 {
-	Eigen::Matrix<double, state_dof, state_dof> cov = Eigen::Matrix<double, state_dof, state_dof>::Identity();
-	// cov.template block<3, 3>(0, 6) = Eigen::Matrix3d::Identity();
-	// vect3 acc_;
-	// in.acc.boxminus(acc_, s.ba);
-	// vect3 omega;
-	// in.gyro.boxminus(omega, s.bg);
-	// cov.template block<3, 3>(6, 3) = -s.rot.toRotationMatrix()*MTK::hat(acc_);
-	// cov.template block<3, 3>(6, 12) = -s.rot.toRotationMatrix();
-	// Eigen::Matrix<state_ikfom::scalar, 2, 1> vec = Eigen::Matrix<state_ikfom::scalar, 2, 1>::Zero();
-	// Eigen::Matrix<state_ikfom::scalar, 3, 2> grav_matrix;
-	// s.S2_Mx(grav_matrix, vec, 15);
-	// cov.template block<3, 2>(6, 15) =  grav_matrix; 
-	// cov.template block<3, 3>(3, 9) = -Eigen::Matrix3d::Identity(); 
+	Eigen::Matrix<double, state_dof, state_dof> cov = Eigen::Matrix<double, state_dof, state_dof>::Zero();
+	cov.template block<3, 3>(0, 3) = -s.bQo.toRotationMatrix() * MTK::hat(in.ovo);
 	return cov;
 }
 
-
 Eigen::Matrix<double, state_dof, process_noise_dof> df_dw(state_ikfom &s, const input_ikfom &in)
 {
-	Eigen::Matrix<double, state_dof, process_noise_dof> cov = Eigen::Matrix<double, state_dof, process_noise_dof>::Identity();
-	// cov.template block<3, 3>(6, 3) = -s.rot.toRotationMatrix();
-	// cov.template block<3, 3>(3, 0) = -Eigen::Matrix3d::Identity();
-	// cov.template block<3, 3>(9, 6) = Eigen::Matrix3d::Identity();
-	// cov.template block<3, 3>(12, 9) = Eigen::Matrix3d::Identity();
+	Eigen::Matrix<double, state_dof, process_noise_dof> cov = Eigen::Matrix<double, state_dof, process_noise_dof>::Zero();
+	cov.template block<3, 3>(0, 0) = s.bQo.toRotationMatrix();
+	cov.template block<3, 3>(3, 3) = Eigen::Matrix3d::Identity();
 	return cov;
 }
 
 measurement_ikfom get_h(state_ikfom &s, bool &valid)
 {
 	measurement_ikfom h_;
-	h_.bpo_meas = s.bpo;	
+	h_.bpo_meas = s.bpo;
 	h_.bQo_meas = s.bQo;
 	return h_;
 }
@@ -106,7 +79,7 @@ measurement_ikfom get_h(state_ikfom &s, bool &valid)
 Eigen::Matrix<double, measurement_dof, state_dof> dh_dx(state_ikfom &s, bool &valid)
 {
 	Eigen::Matrix<double, measurement_dof, state_dof> cov = Eigen::Matrix<double, measurement_dof, state_dof>::Identity();
-	
+
 	return cov;
 }
 
@@ -115,7 +88,5 @@ Eigen::Matrix<double, measurement_dof, measurement_noise_dof> dh_dv(state_ikfom 
 	Eigen::Matrix<double, measurement_dof, measurement_noise_dof> cov = Eigen::Matrix<double, measurement_dof, measurement_noise_dof>::Identity();
 	return cov;
 }
-
-
 
 #endif
--- test_ikfom/src/test_ikfom.cpp
+++ test_ikfom/src/test_ikfom.cpp
@@ -1,47 +1,69 @@
 #include <Eigen/Eigen>
 #include "../include/test_ikfom/use-ikfom.hpp"
 
-#define MAXIMUM_ITER 10  // check the meaning
-int main(int argc, char** argv)
+#define MAXIMUM_ITER 10 // check the meaning
+int main(int argc, char **argv)
 {
   // define variables
-  double dt = 0.01;                                                 // time step
-  state_ikfom init_state;                                           // state vector (struct)
-  input_ikfom u;                                                    // input vector (struct)
-  measurement_ikfom z;                                              // measurement vector (struct)
-  MTK::get_cov<process_noise_ikfom>::type Q = process_noise_cov();  // process noise covariance: Q, an Eigen matrix
+  double dt = 0.01;                                                // time step
+  state_ikfom ekf_state;                                           // state vector (struct)
+  input_ikfom u;                                                   // input vector (struct)
+  measurement_ikfom z;                                             // measurement vector (struct)
+  MTK::get_cov<process_noise_ikfom>::type Q = process_noise_cov(); // process noise covariance: Q, an Eigen matrix
   MTK::get_cov<measurement_noise_ikfom>::type R =
-      measurement_noise_cov();  // measurement noise covariance: R, an Eigen matrix
+      measurement_noise_cov(); // measurement noise covariance: R, an Eigen matrix
   esekfom::esekf<state_ikfom, process_noise_dof, input_ikfom, measurement_ikfom, measurement_noise_dof>::cov init_P;
 
   // initalize variables
-  init_state.bpo << 0.0, 0.0, 0.0;                // initial position
-  init_state.bQo = MTK::SO3<double>::Identity();  // initial orientation
-  u.ovo << 0.0, 0.0, 0.0;                         // input velocity in body frame
-  u.oomegao << 0.0, 0.0, 0.0;                     // input angular velocity in body frame
-  z.bpo_meas << 0.0, 0.0, 0.1;                    // measurement position in base frame
-  z.bQo_meas = MTK::SO3<double>::Identity();      // measurement orientation in base frame
+  ekf_state.bpo << 0.0, 0.0, 0.0;               // initial position
+  ekf_state.bQo = MTK::SO3<double>::Identity(); // initial orientation
+  u.ovo << 0.1, 0.0, 0.0;                       // input velocity in body frame
+  u.oomegao << 0.1, 0.0, 0.0;                   // input angular velocity in body frame
+  // z.bpo_meas << 0.0, 0.0, 0.1;                  // measurement position in base frame
+  // z.bQo_meas = MTK::SO3<double>::Identity();    // measurement orientation in base frame
+  bool valid = false;
+  z = get_h(ekf_state, valid);
 
   // create the filter object
-  esekfom::esekf<state_ikfom, process_noise_dof, input_ikfom, measurement_ikfom, measurement_noise_dof> kf(init_state,
+  esekfom::esekf<state_ikfom, process_noise_dof, input_ikfom, measurement_ikfom, measurement_noise_dof> kf(ekf_state,
                                                                                                            init_P);
 
-  double epsi[state_dof] = { 0.001 };
+  double epsi[state_dof] = {0.001};
   std::fill(epsi, epsi + state_dof,
-            0.001);  // if the absolute of innovation of ekf update is smaller than epso, the update iteration is
-                     // converged
+            0.001); // if the absolute of innovation of ekf update is smaller than epso, the update iteration is
+                    // converged
   kf.init(get_f, df_dx, df_dw, get_h, dh_dx, dh_dv, MAXIMUM_ITER, epsi);
 
-  // predict
-  kf.predict(dt, Q, u);
+  // setup simulator real process
+  state_ikfom true_state;
+  true_state.bpo << 0.3, 0.2, 0.1;                                  // initial position
+  true_state.bQo = Eigen::Quaternion<double>(0.7071, 0.7071, 0, 0); // initial orientation
+  double tf = 10;
+  double t = 0.0; // simulation time
 
-  // update
-  kf.update_iterated(z, R);
+  while (t < tf)
+  {
+    // predict
+    kf.predict(dt, Q, u);
 
-  // print the state
-  state_ikfom cur_state_aft = kf.get_x();
+    // update
+    kf.update_iterated(z, R);
 
-  std::cout << "Estimated state: \n" << cur_state_aft << std::endl;
+    // // print the state
+    // state_ikfom cur_state_aft = kf.get_x();
+
+    // simulate real system
+    true_state.boxplus(get_f(true_state, u), dt);
+    z = get_h(true_state, valid);
+
+    std::cout << "Real state: \n"
+              << true_state << std::endl;
+
+    std::cout << "Estimated state: \n"
+              << kf.get_x() << std::endl;
+
+    t = t + dt;
+  }
 
   return 0;
 }
